7b05839c6c5f434d2428396de8eb6319b28cd9c3
Alex Mc'key <mckey2002@yandex.ru>
2022-12-03 08:48:51 +0300

    Add simple Sample of Main and Test

--- src/SimpleMain.scala
+++ src/SimpleMain.scala
@@ -1,8 +1,6 @@
 import AoC_Lib._
 
-object SimpleMain {
-  def main(args: Array[String]): Unit = {
+object SimpleMain:
+  def main(args: Array[String]): Unit =
     println("Hello world!")
-    println(DataDir)
-  }
-}
+    println(Inputs.DataDir)
--- test/SimpleTest.scala
+++ test/SimpleTest.scala
@@ -1,9 +1,8 @@
 import org.scalatest.funsuite._
 
-class SimpleTest extends AnyFunSuite {
+class SimpleTest extends AnyFunSuite:
   test("Samples") {
     assert(List(1,2,3).length == 3)
     val s = "А роза упала на лапу Азора".replace(" ", "").toLowerCase
     assert(s.reverse == s)
   }
-}
