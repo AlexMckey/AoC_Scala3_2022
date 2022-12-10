b4dfd60cd44c9b6c52713b1937ef5711951a9b4e
Alex Mc'key <mckey2002@yandex.ru>
2022-12-10 10:16:22 +0300

    Day10 some refactoring and simplify

--- src/day10/Day10.scala
+++ src/day10/Day10.scala
@@ -12,42 +12,30 @@ object Day10 extends aocd.Problem(2022, 10, Title = "Cathode-Ray Tube"):
     part2(things)
     ()
 
-  def prep(input: String): Seq[String] =
-    time("\tprep", { input.toStrs })
+  def prep(input: String): Array[(Int,Int)] =
+    time("\tprep", {
+      input.
+        toStrs
+        .foldLeft((0, 1, List.empty[(Int, Int)])) {
+          case ((c, r, acc), op) =>
+            op match
+              case s"noop" => (c + 1, r, (c, r) :: acc)
+              case s"addx $x" => (c + 2, r + x.toInt, (c + 1, r) :: (c, r) :: acc)
+        }._3
+        .reverse
+        .toArray
+    })
 
-  def part1(ops: Seq[String]): Int = part1 {
+  def part1(cycles: Array[(Int,Int)]): Int = part1 {
     val strengthCycles = List(20,60,100,140,180,220)
-    val cyclesRegs = ops.scanLeft(0 -> 1) { case ((cycle, reg), op) =>
-      op match
-        case s"noop" => (cycle + 1) -> reg
-        case s"addx $x" => (cycle + 2) -> (reg + x.toInt)
-    }
-    val res = strengthCycles
-      .map(fc => cyclesRegs
-        .find(c => c._1 == fc - 1 || c._1 == fc - 2))
-      .map(_.map(_._2)
-        .getOrElse(1))
-    strengthCycles
-      .zip(res)
-      .map(_ * _)
-      .sum
+    strengthCycles.map(c => c * cycles(c - 1)._2).sum
   }
 
-  def part2(ops: Seq[String]): String = part2 {
-    val sb = new StringBuilder(240)
-    ops.foldLeft(0, 1) { case ((c, r), op) =>
-      op match
-        case s"noop" =>
-          val newc = c + 1
-          val newr = r
-          if c % 40 >= r - 1 && c % 40 <= r + 1 then sb.append("#") else sb.append(" ")
-          (newc, newr)
-        case s"addx $d" =>
-          val newc = c + 2
-          val newr = r + d.toInt
-          if c % 40 >= r - 1 && c % 40 <= r + 1 then sb.append("#") else sb.append(" ")
-          if (c + 1) % 40 >= r - 1 && (c + 1) % 40 <= r + 1 then sb.append("#") else sb.append(" ")
-          (newc, newr)
-    }
-    sb.toString().grouped(40).mkString("\n","\n","")
+  def part2(cycles: Array[(Int,Int)]): String = part2 {
+    cycles.foldLeft(new StringBuilder(240)) {
+      case (sb, (c, r)) =>
+        if r - 1 to r + 1 contains c % 40 then sb.append("#") else sb.append(".")
+    }.toString()
+      .grouped(40)
+      .mkString("\n", "\n", "")
   }
--- src/day10/day10.sc
+++ src/day10/day10.sc
@@ -2,12 +2,31 @@
 val ins = "addx 15\naddx -11\naddx 6\naddx -3\naddx 5\naddx -1\naddx -8\naddx 13\naddx 4\nnoop\naddx -1\naddx 5\naddx -1\naddx 5\naddx -1\naddx 5\naddx -1\naddx 5\naddx -1\naddx -35\naddx 1\naddx 24\naddx -19\naddx 1\naddx 16\naddx -11\nnoop\nnoop\naddx 21\naddx -15\nnoop\nnoop\naddx -3\naddx 9\naddx 1\naddx -3\naddx 8\naddx 1\naddx 5\nnoop\nnoop\nnoop\nnoop\nnoop\naddx -36\nnoop\naddx 1\naddx 7\nnoop\nnoop\nnoop\naddx 2\naddx 6\nnoop\nnoop\nnoop\nnoop\nnoop\naddx 1\nnoop\nnoop\naddx 7\naddx 1\nnoop\naddx -13\naddx 13\naddx 7\nnoop\naddx 1\naddx -33\nnoop\nnoop\nnoop\naddx 2\nnoop\nnoop\nnoop\naddx 8\nnoop\naddx -1\naddx 2\naddx 1\nnoop\naddx 17\naddx -9\naddx 1\naddx 1\naddx -3\naddx 11\nnoop\nnoop\naddx 1\nnoop\naddx 1\nnoop\nnoop\naddx -13\naddx -19\naddx 1\naddx 3\naddx 26\naddx -30\naddx 12\naddx -1\naddx 3\naddx 1\nnoop\nnoop\nnoop\naddx -9\naddx 18\naddx 1\naddx 2\nnoop\nnoop\naddx 9\nnoop\nnoop\nnoop\naddx -1\naddx 2\naddx -37\naddx 1\naddx 3\nnoop\naddx 15\naddx -21\naddx 22\naddx -6\naddx 1\nnoop\naddx 2\naddx 1\nnoop\naddx -10\nnoop\nnoop\naddx 20\naddx 1\naddx 2\naddx 2\naddx -6\naddx -11\nnoop\nnoop\nnoop"
 val s = ins.split("\n")
 val s"$op $x" = s.last: @unchecked
+
+val cycs = s.foldLeft((0,1,List.empty[(Int, Int)])){
+  case((c, r, acc), op) =>
+  op match
+    case s"noop" => (c+1, r, (c,r) :: acc)
+    case s"addx $x" => (c+2, r + x.toInt, (c+1,r) :: (c,r) :: acc)
+}._3.reverse
+
+cycs.filter(_._1 > 200)
+
+val cs = List(20,60,100,140,180,220)
+cs.map(c => c * cycs(c-1)._2).sum
+
+val sb = cycs.foldLeft(new StringBuilder(140)){
+  case (sb, (c,r)) =>
+    if r-1 to r+1 contains c % 40 then sb.append("#") else sb.append(".")
+}
+sb.toString().grouped(40).mkString("\n","\n","")
+
+
 val cycles = s.scanLeft(0 -> 1){ case((cycle, reg), op) =>
   op match
     case s"noop" => (cycle+1) -> reg
     case s"addx $x" => (cycle+2) -> (reg+x.toInt)
 }
-val cs = List(20,60,100,140,180,220)
 val res = cs.map(fc => cycles.find(c => c._1 == fc-1 || c._1 == fc-2)).map(_.map(_._2).getOrElse(1))
 cs.zip(res).map(_ * _).sum
 cycles.filter(_._1 >= 200)
